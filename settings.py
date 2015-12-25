f64b78854a92a784174e1e7a9ba46adb047f3127
trishnaguha <trishnaguha17@gmail.com>
2015-12-25 21:24:05 +0530

    Some changes to handle users_posts

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@
 # Youtube url and api settings
 
 Youtube_playlistId = " "
-Youtube_base_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1&playlistId="+Youtube_playlistId+"&key="
 Youtube_api_key = " "
+Youtube_base_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1&playlistId="+Youtube_playlistId+"&key="+Youtube_api_key
 Youtube_watch_url = "https://www.youtube.com/watch?v="
 
 
 # Facebook url and token settings
 
-Facebook_base_url = "https://graph.facebook.com/me/feed/?message="
+Facebook_base_url = "https://graph.facebook.com/me/feed/"
 Access_token = " "
